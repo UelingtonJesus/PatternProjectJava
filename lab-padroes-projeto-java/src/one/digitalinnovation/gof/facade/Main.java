2ca59843219612b8bf40783fa7ec2dcaf4f04871
UelingtonJesus <uelingtondejesuss.uj@gmail.com>
2022-05-25 12:33:47 -0300

    Adiciona exemplos de padrões de projeto Singleton, Strategy e Facade

---
+++ lab-padroes-projeto-java/src/one/digitalinnovation/gof/facade/Main.java
@@ -0,0 +1,8 @@
+package one.digitalinnovation.gof.facade;
+
+public class Main {
+    public static void main(String[] args) {
+        Facade facade = new Facade();
+        facade.migrarCliente("gfdg", "343545455");
+    }
+}
